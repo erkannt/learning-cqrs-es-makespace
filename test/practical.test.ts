dabe50e76ae37b729fab8dbfb14e352657589e40
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-03-04 20:39:33 +0000

    Reduce responsibility of practical aggregate to the events it can raise

--- test/practical.test.ts
+++ test/practical.test.ts
@@ -3,17 +3,7 @@ import {practical} from '../src/practical';
 describe('practical', () => {
   describe('when given a JoinPractical command', () => {
     describe('and the practical is in the future and has slots available', () => {
-      describe('and the member has completed the online training', () => {
-        it.todo('returns a MembersSignedUpForPractical event');
-      });
-
-      describe('and the member has NOT completed the online training', () => {
-        it.todo('returns no events');
-      });
-
-      describe('and the member has completed only a different online training', () => {
-        it.todo('returns no events');
-      });
+      it.todo('returns a MembersSignedUpForPractical event');
     });
 
     describe('and the practical is in the past', () => {
@@ -30,15 +20,8 @@ describe('practical', () => {
         memberNumber: 123,
         practicalId: 'foo',
       };
-      const history = [
-        {
-          _type: 'CompletedOnlineTraining' as const,
-          memberNumber: 123,
-          trainingId: 'ultimaker',
-        },
-      ];
 
-      const result = practical(history)(command);
+      const result = practical([])(command);
 
       it('returns no events', () => {
         expect(result).toStrictEqual([]);
