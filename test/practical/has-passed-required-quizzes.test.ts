43030619025f67b32a60b04c665406c2c4f8dc09
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-03-11 14:57:20 +0000

    Incomplete test to tease out missing events and fields

--- test/practical/has-passed-required-quizzes.test.ts
+++ test/practical/has-passed-required-quizzes.test.ts
@@ -1,3 +1,10 @@
+import faker from '@faker-js/faker';
+import * as E from 'fp-ts/Either';
+import {joinPractical} from '../../src/commands';
+import {practicalScheduled} from '../../src/events';
+import {hasPassedRequiredQuizzes} from '../../src/practical/has-passed-required-quizzes';
+import {arbitraryMemberNumber, arbitraryPracticalId} from '../../src/types';
+
 describe('has-passed-required-quizzes', () => {
   describe('when member has passed all quizzes required to attend practical', () => {
     it.todo('returns on right');
@@ -7,7 +14,16 @@ describe('has-passed-required-quizzes', () => {
     it.todo('returns on left');
   });
 
-  describe('when practical requires no quizzes to attend', () => {
-    it.todo('returns on right');
+  describe.skip('when practical requires no quizzes to attend', () => {
+    const practicalId = arbitraryPracticalId();
+    const command = joinPractical(arbitraryMemberNumber(), practicalId);
+    const history = [
+      practicalScheduled(arbitraryPracticalId(), 2, faker.date.future()),
+    ];
+    const result = hasPassedRequiredQuizzes(history)(command);
+
+    it('returns on right', () => {
+      expect(E.isRight(result)).toBe(true);
+    });
   });
 });
