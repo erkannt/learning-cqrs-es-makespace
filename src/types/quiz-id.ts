51d65f11509a748e6eeff0823a5a972a88685429
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-03-11 15:20:16 +0000

    Introduce QuizId

---
+++ src/types/quiz-id.ts
@@ -0,0 +1,19 @@
+import * as t from 'io-ts';
+import faker from '@faker-js/faker';
+
+type QuizIdBrand = {
+  readonly QuizId: symbol; // TODO: Why does tsc complain when this is a unique symbol?
+};
+
+export const QuizIdCodec = t.brand(
+  t.string,
+  // regex from https://developers.google.com/docs/api/how-tos/overview#document_id
+  (input): input is t.Branded<string, QuizIdBrand> =>
+    /[a-zA-Z0-9-_]+/.test(input),
+  'QuizId'
+);
+
+export type QuizId = t.TypeOf<typeof QuizIdCodec>;
+
+export const arbitraryQuizId = (): QuizId =>
+  faker.random.alphaNumeric() as QuizId;
