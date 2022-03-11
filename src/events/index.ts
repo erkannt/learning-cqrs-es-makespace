dbf8a4364a972f6c4f858f168cd55dc1d3ed1f59
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-03-11 16:44:26 +0000

    Add QuizAdded event

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,15 +1,21 @@
 import {MemberSignedUpForPractical} from './member-signed-up-for-practical';
 import {PracticalScheduled} from './practical-scheduled';
+import {QuizPassed} from './quiz-passed';
 
 export {
   PracticalScheduled,
   practicalScheduled,
   PracticalScheduledCodec,
 } from './practical-scheduled';
+
 export {
   MemberSignedUpForPractical,
   MemberSignedUpForPracticalCodec,
   memberSignedUpForPractical,
 } from './member-signed-up-for-practical';
+export {quizPassed, QuizPassed, QuizPassedCodec} from './quiz-passed';
 
-export type Event = MemberSignedUpForPractical | PracticalScheduled;
+export type Event =
+  | MemberSignedUpForPractical
+  | PracticalScheduled
+  | QuizPassed;
---
+++ src/events/quiz-passed.ts
@@ -0,0 +1,20 @@
+import {MemberNumber, MemberNumberCodec} from '../types';
+import * as t from 'io-ts';
+import {QuizId, QuizIdCodec} from '../types/quiz-id';
+
+export const QuizPassedCodec = t.type({
+  _type: t.literal('QuizPassed'),
+  quizId: QuizIdCodec,
+  memberNumber: MemberNumberCodec,
+});
+
+export type QuizPassed = t.TypeOf<typeof QuizPassedCodec>;
+
+export const quizPassed = (
+  quizId: QuizId,
+  memberNumber: MemberNumber
+): QuizPassed => ({
+  _type: 'QuizPassed' as const,
+  quizId,
+  memberNumber,
+});
