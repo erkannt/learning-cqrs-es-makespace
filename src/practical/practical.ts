47a7cb332c8fc8ca56447df5796fc791f720601c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-04-22 10:33:30 +0100

    Separate construction of the practicals state from assertions made against it

--- src/practical/practical.ts
+++ src/practical/practical.ts
@@ -11,8 +11,37 @@ import {
 } from '../events';
 import {JoinPractical} from './join-practical';
 import {SchedulePractical} from './schedule-practical';
+import {MemberNumber, PracticalId} from '../types';
+import {QuizId} from '../types/quiz-id';
 
 type PracticalCommand = SchedulePractical | JoinPractical;
+
+type PracticalState = {
+  id: PracticalId;
+  requiredQuizzes: ReadonlyArray<QuizId>;
+  capacity: Number;
+  date: Date;
+  attendingMembers: ReadonlyArray<MemberNumber>;
+};
+
+const constructStateOfPractical =
+  (history: ReadonlyArray<Event>) => (practicalId: PracticalId) => {
+    const getAttending = pipe(
+      history,
+      RA.filter(MemberSignedUpForPracticalCodec.is),
+      RA.filter(event => event.practicalId === practicalId),
+      RA.map(event => event.memberNumber)
+    );
+
+    const state = pipe(
+      history,
+      RA.filter(PracticalScheduledCodec.is),
+      RA.findFirst(event => event.id === practicalId),
+      O.map(partial => ({...partial, attendingMembers: getAttending}))
+    );
+
+    return state;
+  };
 
 export type Practical = (
   history: ReadonlyArray<Event>
@@ -22,31 +51,18 @@ export const practical: Practical = history => command => {
   switch (command._type) {
     case 'SchedulePractical':
       return [];
+
     case 'JoinPractical': {
-      const getPractical = pipe(
-        history,
-        RA.filter(PracticalScheduledCodec.is),
-        RA.findFirst(event => event.id === command.practicalId)
-      );
-
-      const getAttendingCount = pipe(
-        history,
-        RA.filter(MemberSignedUpForPracticalCodec.is),
-        RA.filter(event => event.practicalId === command.practicalId),
-        RA.map(event => event.memberNumber),
-        RA.size
-      );
-
-      const practicalHasFreeSpacesLeft =
-        (attendingCount: number) => (event: PracticalScheduled) =>
-          event.capacity > attendingCount;
+      const practicalHasFreeSpacesLeft = (practicalState: PracticalState) =>
+        practicalState.capacity > practicalState.attendingMembers.length;
 
       const practicalIsInTheFuture = (event: PracticalScheduled) =>
         new Date() < event.date;
 
       return pipe(
-        getPractical,
-        O.filter(practicalHasFreeSpacesLeft(getAttendingCount)),
+        command.practicalId,
+        constructStateOfPractical(history),
+        O.filter(practicalHasFreeSpacesLeft),
         O.filter(practicalIsInTheFuture),
         O.match(
           () => [],
