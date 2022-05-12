1028e005dff55b35379adbf428eb8badf0e2b9fe
Daniel Haarhoff <daniel@rknt.de>
2022-05-12 13:13:59 +0100

    Sketch out display of available practicals

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,5 +1,24 @@
 export const home = `
-<h1>Hello World</h1>
+<h1>Makespace Practicals</h1>
 
-<a href="schedule-practical">Schedule a practical</a>
+<h2>Available Practicals</h2>
+
+<ul>
+	<li>
+		<h3>Ultimaker 3D printer</h3>
+		<p>Free slots: 3</p>
+		<p>Date: 2022-10-30</p>
+		<p>Time: 16:00 - 17:00</p>
+		<p>Trainer: Alice Smith</p>
+	</li>
+	<li>
+		<h3>Bandsaw</h3>
+		<p>Free slots: 2</p>
+		<p>Date: 2022-12-30</p>
+		<p>Time: 13:00 - 15:00</p>
+		<p>Trainer: Bob Jones</p>
+	</li>
+</ul>
+
+<a href="schedule-practical">Schedule a new practical</a>
 `;
