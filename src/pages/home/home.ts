2f1cc055533866e790adfc18dd65a528afe3237d
Daniel Haarhoff <daniel@rknt.de>
2022-05-12 13:43:49 +0100

    Start extracting view model

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,4 +2,8 @@ import {pipe} from 'fp-ts/lib/function';
 import {renderAvailablePracticals} from './render-available-practicals';
 import {renderPage} from './render-page';
 
-export const home = pipe(renderAvailablePracticals, renderPage);
+export const home = pipe(
+  [{title: 'Bandsaw'}],
+  renderAvailablePracticals,
+  renderPage
+);
--- src/pages/home/render-available-practicals.ts
+++ src/pages/home/render-available-practicals.ts
@@ -1,18 +1,26 @@
-export const renderAvailablePracticals = `
-	<ul>
-		<li>
-			<h3>Ultimaker 3D printer</h3>
-			<p>Free slots: 3</p>
-			<p>When: <time datetime="2011-11-18T14:54">2011-11-18 14:54</time></p>
-			<p>Duration: <time datetime="P2H0M">2h</time></p>
-			<p>Trainer: Alice Smith</p>
-		</li>
-		<li>
-			<h3>Bandsaw</h3>
-			<p>Free slots: 2</p>
-			<p>When: <time datetime="2020-12-18T14:00">2020-12-18 14:00</time></p>
-			<p>Duration: <time datetime="P0H30M">30m</time></p>
-			<p>Trainer: Bob Jones</p>
-		</li>
-	</ul>
-`;
+import {pipe} from 'fp-ts/lib/function';
+import * as RA from 'fp-ts/ReadonlyArray';
+
+type PracticalViewModel = {
+  title: string;
+};
+
+export const renderAvailablePracticals = (
+  practicals: ReadonlyArray<PracticalViewModel>
+) =>
+  pipe(
+    practicals,
+    RA.map(({title}) => `<h3>${title}</h3>`),
+    RA.map(
+      title => `
+				${title}
+				<p>Free slots: 3</p>
+				<p>When: <time datetime="2011-11-18T14:54">2011-11-18 14:54</time></p>
+				<p>Duration: <time datetime="P2H0M">2h</time></p>
+				<p>Trainer: Alice Smith</p>
+			`
+    ),
+    RA.map(content => `<li>${content}</li>`),
+    listItems => listItems.join('\n'),
+    listContent => `<ul>${listContent}<ul>`
+  );
