a1779a86dfb1c856783d59e4c4bdecbba07ebfb5
Daniel Haarhoff <daniel@rknt.de>
2022-05-12 13:35:13 +0100

    Improve time html semantics

--- src/pages/home/render-available-practicals.ts
+++ src/pages/home/render-available-practicals.ts
@@ -3,15 +3,15 @@ export const renderAvailablePracticals = `
 		<li>
 			<h3>Ultimaker 3D printer</h3>
 			<p>Free slots: 3</p>
-			<p>Date: 2022-10-30</p>
-			<p>Time: 16:00 - 17:00</p>
+			<p>When: <time datetime="2011-11-18T14:54">2011-11-18 14:54</time></p>
+			<p>Duration: <time datetime="P2H0M">2h</time></p>
 			<p>Trainer: Alice Smith</p>
 		</li>
 		<li>
 			<h3>Bandsaw</h3>
 			<p>Free slots: 2</p>
-			<p>Date: 2022-12-30</p>
-			<p>Time: 13:00 - 15:00</p>
+			<p>When: <time datetime="2020-12-18T14:00">2020-12-18 14:00</time></p>
+			<p>Duration: <time datetime="P0H30M">30m</time></p>
 			<p>Trainer: Bob Jones</p>
 		</li>
 	</ul>
