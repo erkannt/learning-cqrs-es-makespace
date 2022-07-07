e02576292708c70cc66b530a99397a3bcf8cf696
Daniel Haarhoff <daniel@rknt.de>
2022-07-07 13:12:33 +0100

    Extract form rendering from page rendering

--- src/pages/schedule-practical.ts
+++ src/pages/schedule-practical.ts
@@ -14,40 +14,48 @@ const paramsCodec = t.type({
 
 type Params = t.TypeOf<typeof paramsCodec>;
 
+const renderPage = (form: string) => `
+	<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<meta http-equiv="X-UA-Compatible" content="ie=edge">
+			<title>Schedule Practical</title>
+			<link rel="stylesheet" href="/static/vanilla.css">
+		</head>
+		<body>
+			<h1>Schedule a practical</h1>
+			${form}
+		</body>
+	</html>
+`;
+
 export const schedulePractical = (params: unknown): E.Either<unknown, string> =>
-  pipe(params, paramsCodec.decode, E.bimap(formatValidationErrors, renderForm));
+  pipe(
+    params,
+    paramsCodec.decode,
+    E.bimap(formatValidationErrors, renderForm),
+    E.map(renderPage),
+  );
 
 const renderForm = (params: Params) => `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Schedule Practical</title>
-    <link rel="stylesheet" href="/static/vanilla.css">
-  </head>
-  <body>
-		<h1>Schedule a practical</h1>
+	<form action="" method="post">
+		<fieldset>
+			<label for="title">Title of Practical</label>
+			<input type="text" name="title" value="${params.title}">
 
-		<form action="" method="post">
-			<fieldset>
-				<label for="title">Title of Practical</label>
-				<input type="text" name="title" value="${params.title}">
+			<label for="date">Date and Time:</label>
+			<input type="datetime-local" name="date" value=${params.date.toISOString()}>
 
-				<label for="date">Date and Time:</label>
-				<input type="datetime-local" name="date" value=${params.date.toISOString()}>
+			<label for="capacity">Capacity</label>
+			<input type="number" name="capacity" value="${params.capacity}">
 
-				<label for="capacity">Capacity</label>
-				<input type="number" name="capacity" value="${params.capacity}">
+			<label for="requiredQuizzes[]">Required Quizzes</label>
+			<input type="text" name="requiredQuizzes[]">
+			<input type="text" name="requiredQuizzes[]">
 
-				<label for="requiredQuizzes[]">Required Quizzes</label>
-				<input type="text" name="requiredQuizzes[]">
-				<input type="text" name="requiredQuizzes[]">
-
-				<input type="submit" value="Schedule the practical">
-			</fieldset>
-		</form>
-  </body>
-</html>
+			<input type="submit" value="Schedule the practical">
+		</fieldset>
+	</form>
 `;
