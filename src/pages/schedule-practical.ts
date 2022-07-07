cbaf1b76abf08d5cc082a4667f395eb32f485374
Daniel Haarhoff <daniel@rknt.de>
2022-07-07 13:46:45 +0100

    Fully break up form

--- src/pages/schedule-practical.ts
+++ src/pages/schedule-practical.ts
@@ -56,22 +56,48 @@ const renderForm = (params: Params) => {
 		`;
     case 2:
       return `
-			<form action="" method="post">
+			<form action="" method="get">
 				<fieldset>
 					<input type="hidden" name="title" value="${params.title}">
 
 					<label for="date">Date and Time:</label>
 					<input type="datetime-local" name="date" value=${params.date.toISOString()}>
 
+					<input type="hidden" name="page" value="${params.page + 1}">
+					<input type="submit" value="Next">
+				</fieldset>
+			</form>
+		`;
+    case 3:
+      return `
+			<form action="" method="get">
+				<fieldset>
+					<input type="hidden" name="title" value="${params.title}">
+					<input type="hidden" name="date" value=${params.date.toISOString()}>
+
 					<label for="capacity">Capacity</label>
 					<input type="number" name="capacity" value="${params.capacity}">
 
-					<label for="requiredQuizzes[]">Required Quizzes</label>
-					<input type="text" name="requiredQuizzes[]">
-					<input type="text" name="requiredQuizzes[]">
+					<input type="hidden" name="page" value="${params.page + 1}">
+					<input type="submit" value="Next">
+				</fieldset>
+			</form>
+		`;
+    case 4:
+      return `
+			<h2>Check and confirm</h2>
+			<h3>Title</h3>
+			<p>${params.title}</p>
+			<h3>Date and time</h3>
+			<p>${params.date.toString()}</p>
+			<h3>Capacity</h3>
+			<p>${params.capacity}</p>
+			<form action="" method="post">
+					<input type="hidden" name="title" value="${params.title}">
+					<input type="hidden" name="date" value=${params.date.toISOString()}>
+					<input type="hidden" name="capacity" value="${params.capacity}">
 
 					<input type="submit" value="Schedule the practical">
-				</fieldset>
 			</form>
 			`;
     default:
